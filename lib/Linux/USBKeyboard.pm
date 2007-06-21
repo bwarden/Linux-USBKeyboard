4bb2fa3a53ce4e56724a639c2a0beac4259df51c
ewilhelm <ewilhelm@893ae339-83d3-0310-8885-b8ca96ff9439>
2007-06-21 08:32:15 +0000

    Linux-USBKeyboard - importing starter directories and tree
    git-svn-id: http://svn.scratchcomputing.com/Linux-USBKeyboard/trunk@1462 893ae339-83d3-0310-8885-b8ca96ff9439

---
+++ lib/Linux/USBKeyboard.pm
@@ -0,0 +1,55 @@
+package Linux::USBKeyboard;
+$VERSION = eval{require version}?version::qv($_):$_ for(0.0.1);
+
+use warnings;
+use strict;
+use Carp;
+
+=head1 NAME
+
+Linux::USBKeyboard - 
+
+=head1 SYNOPSIS
+
+=cut
+
+
+
+
+
+=head1 AUTHOR
+
+Eric Wilhelm @ <ewilhelm at cpan dot org>
+
+http://scratchcomputing.com/
+
+=head1 BUGS
+
+If you found this module on CPAN, please report any bugs or feature
+requests through the web interface at L<http://rt.cpan.org>.  I will be
+notified, and then you'll automatically be notified of progress on your
+bug as I make changes.
+
+If you pulled this development version from my /svn/, please contact me
+directly.
+
+=head1 COPYRIGHT
+
+Copyright (C) 2007 Eric L. Wilhelm, All Rights Reserved.
+
+=head1 NO WARRANTY
+
+Absolutely, positively NO WARRANTY, neither express or implied, is
+offered with this software.  You use this software at your own risk.  In
+case of loss, no person or entity owes you anything whatsoever.  You
+have been warned.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+# vi:ts=2:sw=2:et:sta
+1;
